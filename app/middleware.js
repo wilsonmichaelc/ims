0f76a3a247a1985dda42041bc862351618f92d7e
Michael Wilson <wilson.michael.c@gmail.com>
2016-02-24 09:36:01 -0500

    export functions for use in route/api

--- app/middleware.js
+++ app/middleware.js
@@ -1,49 +1,51 @@
 // route middleware to make sure user is logged in
-function isLoggedIn(req, res, next) {
+module.exports = {
+	isLoggedIn: function (req, res, next) {
 
-	// if user is authenticated in the session, carry on
-	if (req.isAuthenticated())
-		return next();
-
-	// if they aren't redirect them to the home page
-	res.redirect('/');
-}
-
-// route middleware to make sure user is logged in AND an administrator
-function isAdmin(req, res, next) {
-
-	// if user is authenticated in the session, carry on
-	if (req.isAuthenticated())
-		if(req.user.Admin){
+		// if user is authenticated in the session, carry on
+		if (req.isAuthenticated())
 			return next();
-		}
 
-	// if they aren't redirect them to the home page
-	res.redirect('/');
-}
+		// if they aren't redirect them to the home page
+		res.redirect('/');
+	},
 
-// route middleware to make sure user is logged in AND a super user
-function isSuperUser(req, res, next) {
+	// route middleware to make sure user is logged in AND an administrator
+	isAdmin: function (req, res, next) {
 
-	// if user is authenticated in the session, carry on
-	if (req.isAuthenticated())
-		if(req.user.SuperUser){
-			return next();
-		}
+		// if user is authenticated in the session, carry on
+		if (req.isAuthenticated())
+			if(req.user.Admin){
+				return next();
+			}
 
-	// if they aren't redirect them to the home page
-	res.redirect('/');
-}
+		// if they aren't redirect them to the home page
+		res.redirect('/');
+	},
 
-// route middleware to make sure user is logged in AND a super user
-function isAdminOrSuper(req, res, next) {
+	// route middleware to make sure user is logged in AND a super user
+	isSuperUser: function (req, res, next) {
 
-	// if user is authenticated in the session, carry on
-	if (req.isAuthenticated())
-		if(req.user.SuperUser || req.user.Admin){
-			return next();
-		}
+		// if user is authenticated in the session, carry on
+		if (req.isAuthenticated())
+			if(req.user.SuperUser){
+				return next();
+			}
 
-	// if they aren't redirect them to the home page
-	res.redirect('/');
-}
+		// if they aren't redirect them to the home page
+		res.redirect('/');
+	},
+
+	// route middleware to make sure user is logged in AND a super user
+	isAdminOrSuper: function (req, res, next) {
+
+		// if user is authenticated in the session, carry on
+		if (req.isAuthenticated())
+			if(req.user.SuperUser || req.user.Admin){
+				return next();
+			}
+
+		// if they aren't redirect them to the home page
+		res.redirect('/');
+	}
+};
