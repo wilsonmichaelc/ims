cbc2d2590ab363ace84ec2ac6cf6a3ef649e6509
Michael Wilson <wilson.michael.c@gmail.com>
2016-02-24 08:45:54 -0500

    added database configuration

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'devel',
+        'password': 'devel'
+    },
+    'database': 'aims',
+    'users_table': 'Users'
+};
