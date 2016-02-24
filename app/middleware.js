7f7e19a9d8518459d9a5515be079945c04409c24
Michael Wilson <wilson.michael.c@gmail.com>
2016-02-24 11:27:51 -0500

    removed function that arent needed

--- app/middleware.js
+++ app/middleware.js
@@ -15,33 +15,7 @@ module.exports = {
 
 		// if user is authenticated in the session, carry on
 		if (req.isAuthenticated())
-			if(req.user.Admin){
-				return next();
-			}
-
-		// if they aren't redirect them to the home page
-		res.redirect('/');
-	},
-
-	// route middleware to make sure user is logged in AND a super user
-	isSuperUser: function (req, res, next) {
-
-		// if user is authenticated in the session, carry on
-		if (req.isAuthenticated())
-			if(req.user.SuperUser){
-				return next();
-			}
-
-		// if they aren't redirect them to the home page
-		res.redirect('/');
-	},
-
-	// route middleware to make sure user is logged in AND a super user
-	isAdminOrSuper: function (req, res, next) {
-
-		// if user is authenticated in the session, carry on
-		if (req.isAuthenticated())
-			if(req.user.SuperUser || req.user.Admin){
+			if(req.user.admin){
 				return next();
 			}
 
