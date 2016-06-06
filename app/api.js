fe3e92d07bad0bcae9ab95a56eccede4de0720ba
Michael Wilson <wilson.michael.c@gmail.com>
2016-06-06 11:53:51 -0400

    added user update endpoint and updated users table column names

--- app/api.js
+++ app/api.js
@@ -15,7 +15,7 @@ module.exports = function(app, passport){
   // we will want this protected so you have to be logged in to visit
   // we will use route middleware to verify this (the isLoggedIn function)
   app.get('/api/users', middleware.isAdmin, function(req, res) {
-    connection.query("SELECT id, name, email, admin, createdAt FROM Users", function(err, rows){
+    connection.query("SELECT id, name, email, admin, locked, created FROM Users", function(err, rows){
       if(err){
         return done(err);
       }else if(rows.length){
@@ -31,7 +31,7 @@ module.exports = function(app, passport){
   // we will use route middleware to verify this (the isLoggedIn function)
   app.get('/api/users/:user', middleware.isAdmin, function(req, res) {
     var id = req.params.user;
-    connection.query("SELECT id, name, email, admin, createdAt FROM Users WHERE id = ? ",[id], function(err, rows){
+    connection.query("SELECT id, name, email, admin, locked, created FROM Users WHERE id = ? ",[id], function(err, rows){
       if(err){
         return done(err);
       }else if(rows.length){
@@ -46,20 +46,17 @@ module.exports = function(app, passport){
   // =====================================
   // we will want this protected so you have to be logged in to visit
   // we will use route middleware to verify this (the isLoggedIn function)
-  app.put('/api/users', function(req, res) {
-    var id = req.body.id;
-    var name = req.body.name;
-    var email = req.body.email;
-    if(id == req.user.id || middleware.isAdmin){
-      connection.query("UPDATE Users SET name = ?, email = ? WHERE id = ?",[name, email, id], function(err, rows){
+  app.put('/api/users/update', function(req, res) {
+    if(req.body.id == req.user.id || middleware.isAdmin){
+      connection.query("UPDATE Users SET name = ?, email = ?, admin = ?, locked = ? WHERE id = ?",[req.body.name, req.body.email, req.body.admin, req.body.locked, req.body.id], function(err, rows){
         if(err){
-          res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+          res.json({"Error" : true, "Message" : "Error executing MySQL query" + err});
         }else{
           res.json({"Error" : false, "Message" : "Profile updated successfully"});
         }
       });
     }else{
-      res.json({"Error" : true, "Message" : "Only admins can update other profiles"});
+      res.json({"Error" : true, "Message" : "Only admins can update other users profiles."});
     }
   });
 
@@ -76,6 +73,26 @@ module.exports = function(app, passport){
           }
         });
       }
+  });
+
+  // =====================================
+  // GET ALL THE BOOKINGS ================
+  // =====================================
+  app.get('/api/bookings', function(req, res) {
+    connection.query("SELECT Bookings.id, Bookings.title, Bookings.start, Bookings.end, Users.name AS user, Instruments.name AS instrument FROM Bookings INNER JOIN Users ON Users.id = Bookings.user INNER JOIN Instruments ON Instruments.id = Bookings.instrument", [], function(err, rows) {
+      if(!err && rows.length){
+        for(var i=0; i<rows.length; i++){
+          if(rows[i].title == ''){
+            rows[i].title = rows[i].instrument;
+          }
+          console.log(rows[i].start);
+          console.log(String(rows[i].start));
+          rows[i].start = String(rows[i].start);
+          rows[i].end = String(rows[i].end);
+        }
+        res.send(rows);
+      }
+    });
   });
 
 };
